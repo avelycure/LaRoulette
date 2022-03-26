db19f780de41eca46e5490aa9f1dd19ef7b9138f
avelycure <van.emelin2012@yandex.ru>
2022-03-26 18:30:39 +0300

    Refactoring code

---
+++ app/src/main/java/com/avelycure/laroulette/RouletteConstants.kt
@@ -0,0 +1,22 @@
+package com.avelycure.laroulette
+
+object RouletteConstants {
+    const val R_TO_D = 57.2958
+    const val MIN_SPEED = 1f
+
+    //for bundle
+    const val PHI = "phi"
+
+    const val DESIRED_WIDTH = 256
+    const val DESIRED_HEIGHT = 256
+
+    const val CIRCLE_PROPORTION = 0.8f
+
+    //text
+    const val H_OFFSET = 10f
+    const val V_OFFSET = 10f
+
+    const val ACCELERATION = 100f
+    const val ROULETTE_RADIUS = 0.03f
+    const val MAX_ROULLETTE_SPEED = 2200f
+}
